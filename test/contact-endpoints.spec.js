4b4a248161f7457740ddaedfc59503c4e39351fa
Sam Parkinson <parkinsonsp42@gmail.com>
2019-09-16 21:25:59 -0400

    Add test for POST

--- test/contact-endpoints.spec.js
+++ test/contact-endpoints.spec.js
@@ -3,5 +3,51 @@ const app = require('../src/app');
 const helpers = require('./test-helpers');
 
 describe.only('Contact Endpoints', function() {
+  let db;
 
+  before('make knex instance', () => {
+    db = knex({
+      client: 'pg',
+      connection: process.env.TEST_DB_URL,
+    });
+    app.set('db', db);
+  });
+
+  after('disconnect from db', () => db.destroy());
+
+  before('cleanup', () => helpers.cleanTables(db));
+
+  afterEach('cleanup', () => helpers.cleanTables(db));
+
+  describe(`POST /api/contact`, () => {
+    it(`sends a new contact form, responding with 201 and the contact`, () => {
+      const newForm = {
+        first_name: 'Test',
+        last_name: 'User',
+        email: 'test@test.com'
+      }
+      return supertest(app)
+        .post(`/api/contact`)
+        .send(newForm)
+        .expect(201)
+        .expect(res => {
+          expect(res.body).to.have.property('id')
+          expect(res.body.first_name).to.eql(newForm.first_name)
+          expect(res.body.last_name).to.eql(newForm.last_name)
+          expect(res.body.email).to.eql(newForm.email)
+        })
+        .expect(res =>
+          db 
+            .from('contact')
+            .select('*')
+            .where({ id: res.body.id })
+            .first()
+            .then(row => {
+              expect(row.first_name).to.eql(newForm.first_name)
+              expect(row.last_name).to.eql(newForm.last_name)
+              expect(row.email).to.eql(newForm.email)
+            })  
+        )
+    });
+  });
 });
