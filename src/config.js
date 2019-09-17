3657a92f3afd8be2d9557a27ddf280f3a252c727
Sam Parkinson <parkinsonsp42@gmail.com>
2019-09-17 14:46:46 -0400

    Update CORS endpoint

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
-  CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'https://localhost:3000',
+  CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'http://localhost:3000',
   DB_URL: process.env.DATABASE_URL || process.env.DB_URL
 }
